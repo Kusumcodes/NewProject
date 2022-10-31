53b8d5831930cfc02ae128ee47ce738dfd79dc9d
Kusum Singh <kusumdei0409@gmail.com>
2022-10-31 15:50:38 +0530

    commit

---
+++ env/main.py
@@ -0,0 +1,59 @@
+import string
+from fastapi import FastAPI
+import connection
+from bson import ObjectId
+from schematics.models import Model
+
+
+def EmailType():
+    pass
+
+def StringType():
+    pass
+
+def IntType():
+    pass
+
+class Customer(Model):
+    cust_id = 1
+    cust_email = ""
+    cust_name = ""
+    cust_age = 18
+# An instance of class User
+newuser = Customer()
+
+# funtion to create and assign values to the instanse of class Customer created
+def create_user(email, username):
+    newuser.cust_id = ObjectId()
+    newuser.cust_email  = email
+    newuser.cust_name = username
+    return dict(newuser)
+
+app = FastAPI()
+
+
+# Our root endpoint
+@app.get("/")
+def index():
+    return {"message": "Welcome to FastAPI World"}
+
+# Signup endpoint with the POST method
+@app.post("/signup/{email}/{username}")
+def addUser(email, username: str):
+    user_exists = False
+    data = create_user(email, username)
+
+    # Covert data to dict so it can be easily inserted to MongoDB
+    dict(data)
+
+    # Checks if an email exists from the collection of users
+    if connection.db.users.find(
+        {'email': data['email']}
+        ).count() > 0:
+        user_exists = True
+        print("Customer Exists")
+        return {"message":"Customer Exists"}
+    # If the email doesn't exist, create the user
+    elif user_exists == False:
+        connection.db.users.insert_one(data)
+        return {"message":"User Created","email": data['email'], "name": data['name']}
---
+++ env/settings.py
@@ -0,0 +1,4 @@
+
+mongodb_uri = "mongodb+srv://kusumcodes:<password>@cluster0.ywzgdru.mongodb.net/?retryWrites=true&w=majority"
+port = 8000  
+
